ad50108e2fc50f6924da7412583e7d2121bc13e8
Julien de Castelnau <decastelnau@wisc.edu>
2020-11-04 23:00:44 -0600

    extra credit -f arg impl

--- main.c
+++ main.c
@@ -7,51 +7,78 @@
  */
 
 #include <stdio.h>
+#include <string.h>
+#include <getopt.h>
 
 #include "graph.h"
 #include "exec.h"
 #include "makefileparser.h"
 #include "makefilerule.h"
 
-int main(int argc, char** argv) {
+FILE* openMakefile(char* filename) {
     FILE* makefile;
-    makefile=fopen("makefile", "r");
-    if (makefile==NULL) { 
+    if (filename != NULL) {
+        makefile = fopen(filename, "r");
+        if (makefile != NULL) return makefile;
+        fprintf(stderr,"ERROR: Invalid makefile specified.\n");
+        exit(EXIT_FAILURE);
+    } else {
+        makefile=fopen("makefile", "r");
+        if (makefile != NULL) return makefile;
         makefile = fopen("Makefile", "r");
-        if (makefile==NULL) {
-            fprintf(stderr,"ERROR: No makefile found and it was not specified on the command line.\n");
-            exit(EXIT_FAILURE);
+        if (makefile != NULL) return makefile;
+        fprintf(stderr,"ERROR: No makefile found and it was not \
+                        specified on the command line.\n");
+        exit(EXIT_FAILURE);
+        
+    }
+}
+
+int main(int argc, char** argv) {
+    int opt = 0;
+    char* filename = NULL;
+    char* targetname = NULL;
+
+    while ((opt = getopt(argc, argv, "-f:")) != -1) {
+        switch ((char)opt) {
+            case 'f':
+                filename = optarg;
+                break;
+            case '?':
+                fprintf(stderr, "ERROR: error parsing command line args");
+                exit(EXIT_FAILURE);
+                break;
+            default:
+                targetname = optarg;
+                break;
         }
     }
+
+    FILE* makefile = openMakefile(filename);
+
     Graph* g = ParseMakefile(makefile);
     BTree* map = g->searchtree;
     bt_print(map);
 
     LinkedList* ordering;
 
-    if (argc <= 1) {
+    if (targetname == NULL) {
         GNode* first_target = ll_get(g->nodes, 0);
         if (first_target == NULL) {
-            fprintf(stderr, "ERROR: No targets specified in makefile.\n");
+            fprintf(stderr, "ERROR: No targets in makefile.\n");
             exit(EXIT_FAILURE);
         } else {
             ordering = topologicalSortFromNode(g, first_target);
         }
     } else {
-        GNode* result = (GNode*)bt_get(g->searchtree, argv[1]);
+        GNode* result = (GNode*)bt_get(g->searchtree, targetname);
         if (result == NULL) {
-            fprintf(stderr, "ERROR: '%s' is not a valid target.\n", argv[1]);
+            fprintf(stderr, "ERROR: '%s' is not a valid target.\n", targetname);
             exit(EXIT_FAILURE);
         } else {
             ordering = topologicalSortFromNode(g, result);
         }
     }
-
-    //newCommandFromString("one two three four five");
-    //Command* c = newCommandFromString("ls -l -a -h");
-    //execCommand(c);
-    //newCommandFromString("hello world < in > out");
-    //newCommandFromString("one two three four five   ");
 
     execRules(ordering, map);
 
