3655c8a04a603c8aefe8365795d7c9dc5617c77a
Michael Noguera <44954999+michaelnoguera@users.noreply.github.com>
2020-11-04 21:41:31 -0600

    Finalize binary tree, update interface, formatting

--- bintree.c
+++ bintree.c
@@ -2,8 +2,10 @@
  * CS 537 Programming Assignment 3 (Fall 2020)
  * @author Michael Noguera
  * @date 11/4/2020
+ *
+ * @file bintree.h
  * @brief Implementation of binary tree with string keys and void* values.
- * @file bintree.c
+ * @version 1.1
  */
 
 #include "bintree.h"
@@ -13,31 +15,37 @@
 #include <stdlib.h>
 #include <string.h>
 
-// This was supposed to be an AVL tree, but it was not maintaining balance, so
-// the rotate code is commented out
-
-// node constructor
+/**
+ * Constructs an empty BTNode (goes in a BTree), with specified values.
+ *
+ * @param[in] key string key identifying this node. CANNOT BE NULL.
+ * @param[in] value value to associate with key. Can be NULL without issue.
+ *
+ * @return pointer to heap-allocated tree node struc
+ */
 static BTNode* initializeNode(const char* key, const void* value) {
     if (key == NULL) {
         perror("NULL is not a valid key");
         exit(EXIT_FAILURE);
     }
-    BTNode* n = (BTNode*)malloc(sizeof(BTNode) + (sizeof(char) * (strlen(key)+1)));
+    BTNode* n =
+      (BTNode*)malloc(sizeof(BTNode) + (sizeof(char) * (strlen(key) + 1)));
     if (n == NULL) {
         perror("Failed to allocate memory for new tree node");
         exit(EXIT_FAILURE);
     }
-    n->key = key;
+    n->key   = key;
     n->value = value;
-    n->bf = 0;
-    n->left = NULL;
+    n->left  = NULL;
     n->right = NULL;
-    //n->height = 0;
 
     return n;
 }
 
-// tree constructor
+/**
+ * Constructs an empty BTree binary tree with default values.
+ * @return pointer to heap-allocated tree
+ */
 BTree* bt_initializeTree() {
     BTree* t = (BTree*)malloc(sizeof(BTree));
     if (t == NULL) {
@@ -48,17 +56,9 @@ BTree* bt_initializeTree() {
     return t;
 }
 
-/*static int heightOfNode(const BTNode* n) {
-    if (n == NULL) return 0;
-    if (heightOfNode(n->left) > heightOfNode(n->right))
-        return heightOfNode(n->left) + 1;
-    else return heightOfNode(n->right) + 1;
-}*/
-
 void bt_print_in_order_helper(const BTNode* n) {
     if (n == NULL) return;
     bt_print_in_order_helper(n->left);
-    //printf("%s (%i) [%i]\n", n->key, heightOfNode(n), heightOfNode(n->left)-heightOfNode(n->right));
     bt_print_in_order_helper(n->right);
 }
 
@@ -66,125 +66,88 @@ void bt_print(const BTree* bt) {
     bt_print_in_order_helper(bt->root);
 }
 
-static BTNode* rightRotate(BTNode* a) {
-
-    if (a == NULL || a->right == NULL) {
-        perror("Invalid rotation requested, involves a null node");
-        exit(EXIT_FAILURE);
-    }
-
-    // move subtrees
-    //BTNode* b = a->left;
-    //a->left = b->right;
-    //b->left = a;
-
-    // fix balance factors
-    //a->bf = heightOfNode(a->left)-heightOfNode(a->right);
-    //b->bf = heightOfNode(b->left)-heightOfNode(b->right);
-    //a->bf = a->bf + 1 - (b->bf < 0 ? b->bf : 0);
-    //b->bf = b->bf + 1 + (a->bf > 0 ? a->bf : 0);
-
-    //assert(a->bf < 2 && a->bf > -2);
-    //assert(b->bf < 2 && b->bf > -2);
-
-    // point to the new top node
-    return a;
-}
-
-static BTNode* leftRotate(BTNode* a) {
-    if (a == NULL || a->left == NULL) {
-        perror("Invalid rotation requested, involves a null node.");
-        exit(EXIT_FAILURE);
-    }
-
-    // move subtrees
-    //BTNode* b = a->right;
-    //a->right = b->left;
-    //b->left = a;
-
-    // fix balance factors
-    //a->bf = heightOfNode(a->left)-heightOfNode(a->right);
-    //b->bf = heightOfNode(b->left)-heightOfNode(b->right);
-    //a->bf = a->bf - 1 - (b->bf > 0 ? b->bf : 0);
-    //b->bf = b->bf - 1 + (a->bf < 0 ? b->bf : 0);
-
-    //assert(a->bf < 2 && a->bf > -2);
-    //assert(b->bf < 2 && b->bf > -2);
-    // point to the new top node
-
-    return a;
-}
-
-// returns the node that is in old's location
-// @param p parent
-// @param n new node
+/**
+ * Recursive case in add operation.
+ *
+ * @details A reminant of a failed attempt to make this an AVL tree, this
+ * function would return the node that should go in p's location. Because this
+ * is not an AVL tree, that is always p.
+ *
+ * @param[in/out] p parent node, currently being traversed
+ * @param[in/out] n new node, to be inserted
+ * @param[out] err is set to 1 if error occurs
+ *
+ * @return pointer to p, or the node that should go in p's old place
+ */
 static BTNode* insertNodeRecursive(BTNode* p, BTNode* n, int* err) {
     // BASE CASE: correct empty location found --> add here
     if (p == NULL) return n;
 
-    // RECURSIVE CASE: recur left or right
-    int cmp = strcmp(n->key, p->key);
-    switch ((0 < cmp) - (cmp < 0)) {  // = sign of cmp
-    case 0:
-        perror("[HELPER] Prevented add with duplicate key, returning 1");
-        *err = 1;  // already a node with this key -> return with error
-        return p;  // do not need to rebalance because nothing happened
-    case -1:
-        p->left = insertNodeRecursive(p->left, n, err);  // recur left
-        p->bf++;
-        break;
-    case 1:
-        p->right = insertNodeRecursive(p->right, n, err);  // recur right
-        p->bf--;
-        break;
+    // RECURSIVE CASE: recur left or right based on sign of strcmp
+    switch ((0 < strcmp(n->key, p->key)) - (strcmp(n->key, p->key) < 0)) {
+        case 0:
+            perror("[HELPER] Prevented add with duplicate key, returning 1");
+            *err = 1; // already a node with this key -> return with error
+            return p; // do not need to rebalance because nothing happened
+        case -1:
+            p->left = insertNodeRecursive(p->left, n, err); // recur left
+            break;
+        case 1:
+            p->right = insertNodeRecursive(p->right, n, err); // recur right
+            break;
     }
 
-    // REBALANCE after successful insert
-    if (n->bf > 1) {
-        if (n->key < p->left->key) {
-            //printf("SINGLE RIGHT ROTATE\n");
-            n = rightRotate(n);
-        } else if (n->key > p->left->key) {
-            //printf("LEFT RIGHT ROTATE\n");
-            n->left = leftRotate(n->left);
-            n = rightRotate(n);
-        }
-    } else if (n->bf < -1) {
-        if (n->key < p->right->key) {
-            //printf("SINGLE LEFT ROTATE\n");
-            n = leftRotate(n);
-        } else if (n->key < p->right->key) {
-            //printf("RIGHT LEFT ROTATE\n");
-            n->right = rightRotate(n->right);
-            n = leftRotate(n);
-        }
-    }
-
-    return p;  // return reference to this portion of the tree so that changes can persist
+    return p; // return reference to this portion of the tree so that changes
+              // can persist
 }
 
+/**
+ * Error handler for add operation, initiates recursive helper
+ * `insertNodeRecursive`.
+ * @see insertNodeRecursive
+ *
+ * @param tree BTree to insert in
+ * @param n node to add
+ *
+ * @return Error value. 0 on success, 1 if there is already a node with the
+ * specified key or key is NULL. Exits with EXIT_FAILURE upon spontaneous
+ * failure.
+ */
 static int insertNode(BTree* tree, BTNode* n) {
+    // Input validation, important because recursive case doesn't check
     if (tree == NULL) {
         perror("NULL is not a valid tree");
         exit(EXIT_FAILURE);
-    }
-    if (n == NULL) {
+    } else if (n == NULL) {
         perror("NULL is not a valid node");
         exit(EXIT_FAILURE);
     }
+
+    // Refuse to add nodes that already have children, as there is no guarantee
+    // that they won't break the tree
     if (n->left != NULL || n->right != NULL) {
         perror("Provided node is already in a tree or has children");
         return 1;
     }
 
-    int err = 0;
+    // Perform insert and detect errors, if any
+    int err    = 0;
     tree->root = insertNodeRecursive(tree->root, n, &err);
     if (err) perror("Tried to insert node with duplicate key, returning 1");
     return err;
 }
 
-// returns 0 on success, or 1 if a node with the specified key already exists
-// in the tree
+/**
+ * Inserts a value identified by a string key to the tree.
+ *
+ * @param tree BTree to insert to
+ * @param key string key for accessing, duplicate keys not allowed
+ * @param value void* value associated with the key
+ *
+ * @return Error value. 0 on success, 1 if there is already a node with the
+ * specified key or key is NULL. Exits with EXIT_FAILURE upon spontaneous
+ * failure.
+ */
 int bt_insert(BTree* tree, const char* key, const void* value) {
     if (key == NULL) {
         perror("Node key cannot be null.");
@@ -192,72 +155,52 @@ int bt_insert(BTree* tree, const char* key, const void* value) {
     }
 
     BTNode* n = initializeNode(key, value);
-    int err = insertNode(tree, n);
+    int err   = insertNode(tree, n);
 
     return err;
 }
 
+/**
+ * Recursive case for get operation.
+ *
+ * @param n current node in traversal
+ * @param t target key
+ *
+ * @return value corresponding to target key, if found, else null
+ */
 static void* getHelper(const BTNode* n, const char* t) {
     // BASE CASE: fell off tree -> not present
     if (n == NULL) return NULL;
 
     // RECURSIVE CASE: recur left or right
     int cmp = strcmp(t, n->key);
-    switch ((0 < cmp) - (cmp < 0)) {  // = sign of cmp
-    case -1:
-        return getHelper(n->left, t);  // recur left
-    case 1:
-        return getHelper(n->right, t);  // recur right
+    switch ((0 < cmp) - (cmp < 0)) { // = sign of cmp
+        case -1:
+            return getHelper(n->left, t); // recur left
+        case 1:
+            return getHelper(n->right, t); // recur right
     }
 
-    // TARGET FOUND
+    // BASE CASE: target found
     return (void*)(n->value);
 }
 
+/**
+ * Retrieves a value identified by a string key from the tree.
+ *
+ * @param tree BTree to search
+ * @param key string key to search for
+ *
+ * @return The value associated wih the node, or NULL if none found.
+ * Note that null does not neccesarily mean failure because it is a valid value
+ * as well.
+ */
 const void* bt_get(const BTree* tree, const char* key) {
-    if (tree == NULL) {
-        perror("NULL is not a valid tree");
-        exit(EXIT_FAILURE);
-    }
-    if (key == NULL) {
-        perror("NULL is not a valid key");
+    if (tree == NULL || key == NULL) {
+        perror("Invalid parameter. Both tree and key must be non-null.");
         exit(EXIT_FAILURE);
     }
 
+    // initate recursive search from tree root and return result
     return getHelper(tree->root, key);
 }
-
-
-/* TODO implement remove if we need it
-BTNode bt_removeHelper(BTNode p, char** key, int err) {
-    // BASE CASE: fell off tree --> not found
-    if (p == NULL) return p;
-
-    // RECURSIVE CASE: recur left or right
-    int cmp = strcmp(key, p->key);
-    switch ((T(0) < cmp) - (cmp < T(0))) {  // = sign of cmp
-        case 0: // found target
-            
-        case -1:
-            p->left = bt_removeHelper(p->left, key, err);  // recur left
-        case 1:
-            p->right = bt_removeHelper(p->right, key, err);  // recur right
-        default:
-
-    }
-
-    tree->root = bt_removeNode(tree->root, key, &err);
-    return err;
-}
-
-int bt_remove(BTree* tree, char** key) {
-    if (key == NULL) {
-        perror("Node key cannot be null.");
-        return 1;
-    }
-
-    int err = 0;
-    tree->root = bt_removeNode(tree->root, key, &err);
-    return err;
-}
-*/
--- bintree.h
+++ bintree.h
@@ -2,8 +2,10 @@
  * CS 537 Programming Assignment 3 (Fall 2020)
  * @author Michael Noguera
  * @date 11/4/2020
- * @brief Implementation of AVL balanced binary tree with string keys and void* values.
+ *
  * @file bintree.h
+ * @brief Implementation of binary tree with string keys and void* values.
+ * @version 1.1
  */
 
 #ifndef _BINTREE_
@@ -11,50 +13,53 @@
 
 #include <stdlib.h>
 
-struct bt_node_t;
-
+/**
+ * Node for BTree binary tree, identified by a unique string key and storing a
+ * void* value.
+ */
 typedef struct bt_node_t {
-    const char* key;    /// pointer to string key
-    const void* value;  /// pointer to whatever value is to be stored
-    int bf;
+    const char* key;   /// pointer to string key
+    const void* value; /// pointer to whatever value is to be stored
     struct bt_node_t* left;
     struct bt_node_t* right;
 } BTNode;
 
-/// Implementation of AVL Balanced Binary Tree
+// Binary tree, holds BTNode structs.
 typedef struct bt_tree_t {
     struct bt_node_t* root;
 } BTree;
 
 /**
- * Constructs a new binary tree 
- * @return a pointer to the BTree
+ * Constructs an empty BTree binary tree with default values.
+ * @return pointer to heap-allocated tree
  */
 BTree* bt_initializeTree();
 
 /**
  * Inserts a value identified by a string key to the tree.
- * 
+ *
  * @param tree BTree to insert to
  * @param key string key for accessing, duplicate keys not allowed
- * @param value void* value associated with the key 
- * 
- * @return 0 on success, 1 if there is already a node with the specified key.
- * Exits with EXIT_FAILURE upon spontaneous failure.
+ * @param value void* value associated with the key
+ *
+ * @return Error value. 0 on success, 1 if there is already a node with the
+ * specified key or key is NULL. Exits with EXIT_FAILURE upon spontaneous
+ * failure.
  */
 int bt_insert(BTree* tree, const char* key, const void* value);
 
 /**
  * Retrieves a value identified by a string key from the tree.
- * 
+ *
  * @param tree BTree to search
  * @param key string key to search for
- * 
+ *
  * @return The value associated wih the node, or NULL if none found.
  * Note that null does not neccesarily mean failure because it is a valid value
  * as well.
  */
 const void* bt_get(const BTree* tree, const char* key);
 
+/// Prints out string keys in-order from root
 void bt_print(const BTree* bt);
 #endif
